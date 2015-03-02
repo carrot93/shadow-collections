949f1721fdbe8cf590da3c95c6989b5f5927c5fb
Kestanous <CMTopus@me.com>
2015-03-01 21:08:04 -0500

    moved from shadow to mreaction

--- package.js
+++ package.js
@@ -1,12 +1,11 @@
 Package.describe({
-  name: 'mreaction:collections',
+  name: 'mreaction:shadow-collections',
   summary: 'Create Mongo.Collections shadow objects and helpers',
   version: "0.1.0",
   git: "https://github.com/Meteor-Reaction/shadow-collection.git"
 });
 
 Package.on_use(function (api) {
-  api.use('matb33:collection-hooks@0.7.9');
    api.use([
     'underscore@1.0.0', 
     'mongo@1.0.0'

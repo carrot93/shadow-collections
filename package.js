6c2c884436a0f57bd100c8938cc74718aa8eba06
Kestanous <CMTopus@me.com>
2015-03-03 10:38:38 -0500

    support user accounts

--- package.js
+++ package.js
@@ -1,18 +1,22 @@
 Package.describe({
   name: 'mreaction:shadow-collections',
   summary: 'Create Mongo.Collections shadow objects and helpers',
-  version: "0.1.0",
+  version: "0.1.1",
   git: "https://github.com/Meteor-Reaction/shadow-collection.git"
 });
 
 Package.on_use(function (api) {
-   api.use([
+
+  api.use('accounts-base', ['server', 'client'], {weak: true})
+
+  api.use([
     'underscore@1.0.0', 
     'mongo@1.0.0'
-    ]);
-   
-   api.add_files([
+  ]);
+
+  api.add_files([
     'lib/shadow-collection.js'
   ]);
-   api.export('addShadow')
+
+  api.export('addShadow')
 });

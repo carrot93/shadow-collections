81c68f33ab450217f31efc6602794e7b62c32c7c
Kestanous <CMTopus@me.com>
2015-03-13 15:16:35 -0400

    support collection hooks, bump

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'mreaction:shadow-collections',
   summary: 'Create Mongo.Collections shadow objects and helpers',
-  version: "0.1.1",
+  version: "0.2.0",
   git: "https://github.com/Meteor-Reaction/shadow-collection.git"
 });
 
 Package.on_use(function (api) {
 
-  api.use('accounts-base', ['server', 'client'], {weak: true})
+  api.use(['accounts-base', 'matb33:collection-hooks'], ['server', 'client'], {weak: true})
 
   api.use([
     'underscore@1.0.0', 
